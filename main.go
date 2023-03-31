d1592c6085b6886774516bb799adf3149cc803f3
atony2099 <atony2099@gmail.com>
2023-03-31 12:05:20 +0800

    refactor code

--- main.go
+++ main.go
@@ -28,122 +28,50 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func watchFiles(ctx context.Context, dir string, watcher *fsnotify.Watcher) {
-
-	go func() {
-		for {
-			select {
-			case event := <-watcher.Events:
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					log.Println("Modified file:", event.Name)
-					refreshBrowser()
-				}
-			case err := <-watcher.Errors:
-				if err != nil {
-					log.Println("Watcher error:", err)
-				}
-			case <-ctx.Done():
-				return
-			}
-		}
-	}()
-
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			log.Println("Walker error:", err)
-			return err
-		}
-		if info.IsDir() {
-			err = watcher.Add(path)
-			if err != nil {
-				log.Println("Watcher error:", err)
-			}
-		}
-		return nil
-	})
-	if err != nil {
-		log.Fatal("Walker error:", err)
-	}
-
+// Config holds the configuration values
+type Config struct {
+	startingPort int
+	dir          string
 }
 
-func startServer(ctx context.Context, dir string, port int) {
-	server := &http.Server{
-		Addr:    ":" + strconv.Itoa(port),
-		Handler: http.DefaultServeMux,
-	}
+// FileWatcher watches for file changes
+type FileWatcher struct {
+	watcher *fsnotify.Watcher
+}
 
-	http.HandleFunc("/", handleRequest(dir))
-	http.HandleFunc("/ws", handleWebSocket)
+// WebSocketClients holds connected WebSocket clients
+type WebSocketClients struct {
+	clients map[*websocket.Conn]bool
+}
 
-	// Start the server in a separate goroutine
-	go func() {
-		err := server.ListenAndServe()
-		if err != http.ErrServerClosed {
-			log.Fatalf("Server error: %v", err)
-		}
-	}()
-
-	log.Printf("Serving %s on HTTP port: %d\n", dir, port)
-
-	// Wait for the context to be cancelled
-	<-ctx.Done()
-
-	// Shut down the server gracefully
-	ctxShutdown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	server.Shutdown(ctxShutdown)
-
+// Server represents an HTTP server
+type Server struct {
+	server *http.Server
 }
 
 func main() {
 
-	var startingPort int
-	var dir string
-	flag.IntVar(&startingPort, "port", 8080, "Starting port for the HTTP server")
-	flag.StringVar(&dir, "dir", "./", "Directory to serve")
-	flag.Parse()
+	cfg := parseFlags()
 
 	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	port, err := findAvailablePort(startingPort)
+	port, err := findAvailablePort(cfg.startingPort)
 	if err != nil {
 		log.Fatal("Could not find an available port")
 	}
 
-	watcher, err := fsnotify.NewWatcher()
-	defer watcher.Close()
+	fileWatcher, err := NewFileWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer fileWatcher.Close()
 
-	go startServer(ctx, dir, port)
-	go watchFiles(ctx, dir, watcher)
+	webSocketClients := NewWebSocketClients()
+	server := NewServer(cfg.dir, port, webSocketClients)
 
-	go func() {
-		for {
-			select {
-			case event := <-watcher.Events:
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					log.Println("Modified file:", event.Name)
-					refreshBrowser()
-				}
-			case err := <-watcher.Errors:
-				log.Println("Error:", err)
-			}
-		}
-	}()
-
-	// Watch directory and its subdirectories
-	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			return watcher.Add(path)
-		}
-		return nil
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
+	go server.Run(ctx)
+	go fileWatcher.WatchFiles(ctx, cfg.dir, webSocketClients)
 
 	url := fmt.Sprintf("http://localhost:%d", port)
 	if err := browser.OpenURL(url); err != nil {
@@ -151,13 +79,23 @@ func main() {
 	}
 
 	// Wait for exit signal
+	waitForExitSignal()
+	log.Println("Received exit signal, shutting down server...")
+
+}
+
+func waitForExitSignal() {
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
 	<-signalChan
-	log.Println("Received exit signal, shutting down server...")
-	cancel()
-	time.Sleep(time.Second)
+}
 
+func parseFlags() Config {
+	var cfg Config
+	flag.IntVar(&cfg.startingPort, "port", 8080, "Starting port for the HTTP server")
+	flag.StringVar(&cfg.dir, "dir", "./", "Directory to serve")
+	flag.Parse()
+	return cfg
 }
 
 func findAvailablePort(startingPort int) (int, error) {
@@ -177,30 +115,36 @@ func findAvailablePort(startingPort int) (int, error) {
 }
 
 func handleRequest(dir string) func(w http.ResponseWriter, r *http.Request) {
-
 	return func(w http.ResponseWriter, r *http.Request) {
-		// Serve files and inject WebSocket code into HTML files
-		filePath := filepath.Join(dir, filepath.Clean(r.URL.Path))
 
-		fileInfo, err := os.Stat(filePath)
+		serveFileWithWebSocketInjection(w, r, dir)
+	}
+
+}
+
+func serveFileWithWebSocketInjection(w http.ResponseWriter, r *http.Request, dir string) {
+
+	// Serve files and inject WebSocket code into HTML files
+	filePath := filepath.Join(dir, filepath.Clean(r.URL.Path))
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	if fileInfo.IsDir() {
+		filePath = filepath.Join(filePath, "index.html")
+	}
+
+	if strings.HasSuffix(filePath, ".html") {
+		content, err := ioutil.ReadFile(filePath)
 		if err != nil {
 			http.NotFound(w, r)
 			return
 		}
 
-		if fileInfo.IsDir() {
-			filePath = filepath.Join(filePath, "index.html")
-		}
-
-		if strings.HasSuffix(filePath, ".html") {
-			content, err := ioutil.ReadFile(filePath)
-			if err != nil {
-				http.NotFound(w, r)
-				return
-			}
-
-			// Inject WebSocket code into the HTML file
-			injectedContent := strings.Replace(string(content), "</body>", `<script>
+		// Inject WebSocket code into the HTML file
+		injectedContent := strings.Replace(string(content), "</body>", `<script>
 let socket = new WebSocket("ws://" + location.host + "/ws");
 socket.onmessage = function(event) {
 	if (event.data === "reload") {
@@ -209,17 +153,34 @@ socket.onmessage = function(event) {
 };
 </script></body>`, 1)
 
-			w.Header().Set("Content-Type", "text/html; charset=utf-8")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(injectedContent))
-		} else {
-			http.ServeFile(w, r, filePath)
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(injectedContent))
+	} else {
+		http.ServeFile(w, r, filePath)
+	}
+
+}
+
+func NewWebSocketClients() *WebSocketClients {
+	return &WebSocketClients{
+		clients: make(map[*websocket.Conn]bool),
+	}
+}
+
+// refreshBrowser sends refresh messages to connected WebSocket clients
+func (wsc *WebSocketClients) refreshBrowser() {
+	for client := range wsc.clients {
+		err := client.WriteMessage(websocket.TextMessage, []byte("reload"))
+		if err != nil {
+			log.Printf("WebSocket error: %v", err)
+			client.Close()
+			delete(wsc.clients, client)
 		}
 	}
 }
 
-func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-	// Upgrade the HTTP connection to a WebSocket connection
+func (wsc *WebSocketClients) handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Printf("Failed to upgrade to WebSocket: %v", err)
@@ -227,11 +188,22 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
-	// Add the connection to the clients map
-	clients[conn] = true
-	defer delete(clients, conn)
+	wsc.addClient(conn)
+	defer wsc.removeClient(conn)
 
-	// Keep the WebSocket connection alive by reading messages
+	wsc.keepWebSocketConnectionAlive(conn)
+
+}
+
+func (wsc *WebSocketClients) addClient(conn *websocket.Conn) {
+	wsc.clients[conn] = true
+}
+
+func (wsc *WebSocketClients) removeClient(conn *websocket.Conn) {
+	delete(wsc.clients, conn)
+}
+
+func (wsc *WebSocketClients) keepWebSocketConnectionAlive(conn *websocket.Conn) {
 	for {
 		_, _, err := conn.ReadMessage()
 		if err != nil {
@@ -240,14 +212,91 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func refreshBrowser() {
-	// Send refresh message to connected WebSocket clients
-	for client := range clients {
-		err := client.WriteMessage(websocket.TextMessage, []byte("reload"))
-		if err != nil {
-			log.Printf("WebSocket error: %v", err)
-			client.Close()
-			delete(clients, client)
-		}
+// NewFileWatcher creates a new FileWatcher instance
+func NewFileWatcher() (*FileWatcher, error) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return nil, err
 	}
+
+	return &FileWatcher{watcher: watcher}, nil
+}
+
+// Close closes the FileWatcher instance
+func (fw *FileWatcher) Close() {
+	fw.watcher.Close()
+}
+
+// WatchFiles watches for file changes in the given directory
+func (fw *FileWatcher) WatchFiles(ctx context.Context, dir string, wsClients *WebSocketClients) {
+	go func() {
+		for {
+			select {
+			case event := <-fw.watcher.Events:
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("Modified file:", event.Name)
+					wsClients.refreshBrowser()
+				}
+			case err := <-fw.watcher.Errors:
+				if err != nil {
+					log.Println("Watcher error:", err)
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Println("Walker error:", err)
+			return err
+		}
+		if !info.IsDir() && isHTMLorCSSorJSFile(path) {
+			err = fw.watcher.Add(path)
+			if err != nil {
+				log.Println("Watcher error:", err)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal("Walker error:", err)
+	}
+}
+
+func isHTMLorCSSorJSFile(filePath string) bool {
+	ext := filepath.Ext(filePath)
+	return ext == ".html" || ext == ".css" || ext == ".js"
+}
+
+// NewServer creates a new Server instance
+func NewServer(dir string, port int, wsClients *WebSocketClients) *Server {
+	server := &http.Server{
+		Addr:    ":" + strconv.Itoa(port),
+		Handler: http.DefaultServeMux,
+	}
+
+	http.HandleFunc("/", handleRequest(dir))
+	http.HandleFunc("/ws", wsClients.handleWebSocket)
+
+	return &Server{server: server}
+}
+
+// Run starts the HTTP server
+func (s *Server) Run(ctx context.Context) {
+	go func() {
+		err := s.server.ListenAndServe()
+		if err != http.ErrServerClosed {
+			log.Fatalf("Server error: %v", err)
+		}
+	}()
+
+	log.Printf("Serving on: http://localhost%s\n", s.server.Addr)
+
+	<-ctx.Done()
+
+	ctxShutdown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	s.server.Shutdown(ctxShutdown)
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -8,73 +10,115 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+	"time"
 )
 
-func createTestServer(dir string) *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handleRequest(dir))
-	mux.HandleFunc("/ws", handleWebSocket)
-	return mux
+// Existing test cases
+
+func TestServeFileWithWebSocketInjection(t *testing.T) {
+	// Create a temporary HTML file
+	root := "./"
+	tempFile, err := ioutil.TempFile(root, "test-*.html")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tempFile.Name())
+
+	// Write content to the temporary HTML file
+	htmlContent := `<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Test HTML</title>
+</head>
+<body>
+	<h1>Hello, World!</h1>
+</body>
+</html>`
+	_, err = tempFile.WriteString(htmlContent)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tempFile.Close()
+
+	// Create a test request
+	req, err := http.NewRequest("GET", "/"+filepath.Base(tempFile.Name()), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a response recorder
+	rr := httptest.NewRecorder()
+
+	// Call serveFileWithWebSocketInjection
+	serveFileWithWebSocketInjection(rr, req, root)
+
+	// Check if the WebSocket injection code is present in the response body
+	injectedContent := rr.Body.String()
+
+	if !strings.Contains(injectedContent, "let socket = new WebSocket") {
+		t.Error("WebSocket injection code not found in the served HTML file")
+	}
 }
 
-func TestHandleRequest(t *testing.T) {
-	tmpDir, err := ioutil.TempDir("", "test-server")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(tmpDir)
+func TestParseFlags(t *testing.T) {
+	cfg := parseFlags()
 
-	indexContent := "<!DOCTYPE html><html><head></head><body>Hello, World!</body></html>"
-	err = ioutil.WriteFile(filepath.Join(tmpDir, "index.html"), []byte(indexContent), 0644)
-	if err != nil {
-		t.Fatal(err)
+	if cfg.startingPort != 8080 {
+		t.Errorf("Expected startingPort to be 8080, but got %d", cfg.startingPort)
 	}
 
-	server := createTestServer(tmpDir)
-	testServer := httptest.NewServer(server)
-	defer testServer.Close()
+	if cfg.dir != "./" {
+		t.Errorf("Expected dir to be './', but got %s", cfg.dir)
+	}
+}
 
-	resp, err := http.Get(testServer.URL)
+func TestRunServer(t *testing.T) {
+	// Create a new temporary directory to serve files from
+	dir, err := ioutil.TempDir("", "fileserver-test")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("Failed to create temporary directory: %v", err)
+	}
+	defer os.RemoveAll(dir)
+
+	// Create a new file to serve
+	filePath := filepath.Join(dir, "test.html")
+	err = ioutil.WriteFile(filePath, []byte("Hello, World!"), 0644)
+	if err != nil {
+		t.Fatalf("Failed to create file: %v", err)
+	}
+
+	// Start the server in a separate goroutine
+	cfg := Config{startingPort: 8080, dir: dir}
+	wsc := NewWebSocketClients()
+	server := NewServer(cfg.dir, cfg.startingPort, wsc)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go server.Run(ctx)
+
+	// Wait for the server to start up
+	time.Sleep(100 * time.Millisecond)
+
+	// Make a request to the server and verify the response
+	resp, err := http.Get(fmt.Sprintf("http://localhost:%d/test.html", cfg.startingPort))
+	if err != nil {
+		t.Fatalf("Failed to make HTTP request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		t.Errorf("Expected status code 200, got %d", resp.StatusCode)
+		t.Errorf("Expected status code to be %v, but got %v", http.StatusOK, resp.StatusCode)
 	}
 
+	expectedContent := "Hello, World!"
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("Failed to read response body: %v", err)
 	}
 
-	expectedBody := "<!DOCTYPE html><html><head></head><body>Hello, World!<script>let socket = new WebSocket(\"ws://\" + location.host + \"/ws\");socket.onmessage = function(event) {if (event.data === \"reload\") {location.reload();}};</script></body></html>"
-
-	got := strings.ReplaceAll(string(body), "\n", "")
-	got = strings.ReplaceAll(got, "\t", "")
-	want := strings.ReplaceAll(expectedBody, "\n", "")
-	want = strings.ReplaceAll(want, "\t", "")
-
-	if got != want {
-		t.Errorf("Unexpected response body:\nGot: %s\nWant: %s", string(body), expectedBody)
-	}
-}
-
-func TestFindAvailablePort(t *testing.T) {
-	tests := []struct {
-		startingPort int
-		expectedPort int
-	}{
-		{8080, 8080},
-		{8081, 8081},
-		{0, -1}, // test when no ports are available
+	if !strings.Contains(string(body), expectedContent) {
+		t.Errorf("Expected body to contain '%v', but got '%v'", expectedContent, string(body))
 	}
 
-	for _, tt := range tests {
-		port, _ := findAvailablePort(tt.startingPort)
-		if port != tt.expectedPort {
-			t.Errorf("findAvailablePort(%d) = %d, expected %d", tt.startingPort, port, tt.expectedPort)
-		}
-	}
 }
